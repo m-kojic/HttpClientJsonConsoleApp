cb0f5f14a9560cbf4c87097c149b2e9562c94686
Milos Kojic <m.kojic@live.com>
2020-03-03 12:28:15 +0000

    HttpJsonConsoleApp

---
+++ HttpJsonConsoleApp/CommentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HttpJsonConsoleApp
+{
+    public class CommentModel
+    {
+        public int postId;
+        public int id;
+        public string name;
+        public string email;
+        public string body;
+    }
+}
---
+++ HttpJsonConsoleApp/ToDoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HttpJsonConsoleApp
+{
+    // https://jsonplaceholder.typicode.com/todos/
+    public class ToDoModel
+    {
+        public int userId;
+        public int id;
+        public string title;
+        public bool completed;
+    }
+}
